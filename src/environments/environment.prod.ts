55eb64845629185e6dfd9c845b24ecf31064b879
Roberto Nogueira <robertonogueira0507@gmail.com>
2026-07-29 22:33:53 +0100

    New changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  apiUrl: 'https://sistema-gestao-api-production.up.railway.app',
+  apiUrl: 'https://sistema-gestao-api-production.up.railway.app:8080',
 
   httpOptions: {
     headers: new HttpHeaders({
